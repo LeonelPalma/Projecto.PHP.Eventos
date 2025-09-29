046a4d61c31ad0a26b46e2e98f2702e64a98f198
LeonelPalma <leonelbpalma@gmail.com>
2025-09-29 13:43:32 +0100

    erros a resolver

--- Eventos/wp-content/plugins/eventos-plugin/eventos.php
+++ Eventos/wp-content/plugins/eventos-plugin/eventos.php
@@ -10,13 +10,17 @@
  * Implementar um shortcode chamado [eventos_futuros], responsável por listar os eventos cuja data seja igual ou superior à atual. Esse shortcode deverá aceitar um parâmetro opcional limite, permitindo restringir o número de eventos apresentados. A listagem deverá ser apresentada sob a forma de grelha de três colunas.
  * O plugin deverá ainda incluir um template single para os eventos, onde serão apresntados o título, a data, o local, o organizador, a imagem de destaque e o conteúdo do evento.
  */
- 
+
+if (!defined('ABSPATH')){
+    exit;  // Supostamente necessário! Fonte: Tutorial 
+}
+
 /*-- Registar Costum Post Type (CPT) --*/
 function registo_eventos__cpt()  // Chamar a função ititulada "registo_eventos__cpt"
 {
     $labels = array(
-        "name" => __("Eventos", "eventos"),
-        "singular_name" => __("Evnto", "eventos"),
+        "name" => __("Eventos", "eventos"),  // Opção por "eventos" vs "textdomain"
+        "singular_name" => __("Evento", "eventos"),
         "add_new" => __("Adicionar Novo", "eventos"),
         "add_new_item" => __("Adicionar Novo Evento", "eventos"),
         "edit_item" => __("Editar Evento", "eventos"),
@@ -26,6 +30,7 @@ function registo_eventos__cpt()  // Chamar a função ititulada "registo_eventos__
         "menu_name" => __("Eventos", "eventos"),
     );
 
+    // Requisitos do CPT
     $args = array(
         'label'=> __('Eventos', 'eventos'),
         'labels'=> $labels,
@@ -33,29 +38,75 @@ function registo_eventos__cpt()  // Chamar a função ititulada "registo_eventos__
         'show_in_rest'=> true,
         'has_archive'=> true,
         'supports'=> array('title','editor','thumbnail'),
-        'rewrite'=> array('slug' => 'eventos'),);
-register_post_type('evento', $args );
+        'rewrite'=> array('slug' => 'eventos'),
+    );
+    register_post_type('evento', $args );
 }
 add_action('init','registo_eventos__cpt');
 
+// Campos Personalizados
+if(function_exists('acf_add_local_field_group')){
+    acf_add_local_group(array(
+        'key'=> 'grupo_eventos',
+        'title'=> 'Detalhes do Evento',
+        'fields'=> array(
+            array(
+                'key'=> 'data_evento',
+                'label'=> 'Data do Evento',
+                'name'=> 'data_evento',
+                'type'=> 'date_picker',
+                'required'=> 1,
+            ),
+            array(
+                'key'=> 'local_evento',
+                'label'=> 'Local do Evento',
+                'name'=> 'local_evento',
+                'type'=> 'text',
+                'required'=> 1,
+            ),
+            array(
+                'key'=> 'organizador_evento',
+                'label'=> 'Organizador do Evento',
+                'name'=> 'organizador_evento',
+                'type'=> 'Text',
+                'required'=> 1,
+            ),
+        ),
+        'location'=> array(
+            array(
+                array(
+                'param'=> 'post_type',
+                'operator'=> '==',
+                'value'=> 'eventos',
+                ),
+            ),
+        ),
+    ));
+}
 
 /*-- Shortcode: [eventos_futuros threshold="3"] --*/ 
 function eventos_futuros_shortcode($atts) {
     $atts = shortcode_atts( array(
         'threshold'=> -1,  // Sem limite
-        ), $atts,'eventos_futuros');
-    $today = current_time('Y-m-d');  // Memória da data atual
+        ), 
+        $atts,'eventos_futuros'
+        );
+    
+    $data_atual = current_time('Y-m-d');  // Memória da data atual
+    
     $query_args = array(
         'post_type'=> 'evento',
         'posts_per_page'=> intval( $atts['threshold'] ),
         'meta_key'=> 'event_date',
         'meta_value'=> '$today',
         'meta_compare'=> '>=',
-        'meta_type'=> 'meta_value',
+        'meta_type'=> 'DATE',
+        'orderby'=> 'meta_value',
         'order'=> 'ASC',
     );  // Data igual ou superior à atual
-    $events = new WP_Query($query_args);
-    if ( $events->have_posts() ) {
+    
+    $eventos = new WP_Query($query_args);
+    if ( $eventos->have_posts() ) {
         return '<p>Sem Eventos Futuros.</p>';
         }
     /* Output */
